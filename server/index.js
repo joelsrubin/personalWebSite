e7fdb31d614e1d890fe561ebc81d065e86b0e462
joelsrubin <joelsrubin@gmail.com>
2021-02-01 17:13:00 -0600

    https added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const fs = require('fs')
+const https = require('https')
 const PORT = process.env.PORT
+
+var privateKey = fs.readFileSync('./assets/generated-private-key.txt')
+var certificate = fs.readFileSync('./assets/generated-csr.txt')
+
+var options = {
+  key: privateKey,
+  cert: certificate
+}
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`
 })
