bd18cf6538d2d79c819c617183f5091b38e98bf7
joelsrubin <joelsrubin@gmail.com>
2021-02-03 12:40:34 -0600

    drag functionality

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,7 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 import styled, { createGlobalStyle, keyframes } from 'styled-components';
 
 export default function App() {
+  let start,
+    end,
+    delta = 0.11;
+  let start2,
+    end2,
+    delta2 = 0.11;
+
+  const measureClick = (e) => {
+    let button = document.getElementById('button1');
+    button.addEventListener('mousedown', () => {
+      start = new Date();
+    });
+    button.addEventListener('mouseup', () => {
+      end = new Date();
+      delta = (end - start) / 1000.0;
+    });
+    if (delta > 0.1) {
+      e.preventDefault();
+    }
+  };
+  const measureClick2 = (e) => {
+    let button2 = document.getElementById('button2');
+    button2.addEventListener('mousedown', () => {
+      start2 = new Date();
+    });
+    button2.addEventListener('mouseup', () => {
+      end2 = new Date();
+      delta2 = (end2 - start2) / 1000.0;
+    });
+    if (delta2 > 0.1) {
+      e.preventDefault();
+    }
+  };
   return (
     <>
       <GlobalStyle />
@@ -10,15 +45,28 @@ export default function App() {
       </Header>
       <Body>
         <ButtonContainer>
-          <form target='_blank' action='https://github.com/joelsrubin'>
-            <Button2 github>Github</Button2>
-          </form>
-          <form
-            target='_blank'
-            action='https://www.linkedin.com/in/joel-rubin-0529'
-          >
-            <Button>LinkedIn</Button>
-          </form>
+          <Draggable>
+            <form
+              id='button1'
+              onSubmit={measureClick}
+              target='_blank'
+              action='https://github.com/joelsrubin'
+            >
+              <Button2 id='mydiv' github>
+                Github
+              </Button2>
+            </form>
+          </Draggable>
+          <Draggable>
+            <form
+              id='button2'
+              target='_blank'
+              action='https://www.linkedin.com/in/joel-rubin-0529'
+              onSubmit={measureClick2}
+            >
+              <Button id='link'>LinkedIn</Button>
+            </form>
+          </Draggable>
         </ButtonContainer>
       </Body>
     </>
@@ -33,6 +81,7 @@ const GlobalStyle = createGlobalStyle`
     background-color: cornflowerblue;
     color: #c6d4df;
     font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+    position: relative;
   }
 `;
 
@@ -69,6 +118,8 @@ const Button = styled.button`
   border-radius: 30px;
   height: 100px;
   width: 100px;
+  position: absolute;
+  z-index: 9;
   /* border: none; */
   outline: none;
   background-color: ${(props) =>
@@ -77,6 +128,7 @@ const Button = styled.button`
 
   :active {
     background-color: ${(props) => (props.github ? '#9a4747' : '#357b35')};
+    animation-play-state: paused;
   }
 
   animation: ${rotate} 10s linear infinite;
@@ -94,6 +146,9 @@ const Button2 = styled.button`
   border-radius: 30px;
   height: 100px;
   width: 100px;
+  position: absolute;
+  z-index: 9;
+
   /* border: none; */
   outline: none;
   background-color: ${(props) =>
--- server/index.js
+++ server/index.js
@@ -2,17 +2,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
-const fs = require('fs');
-const https = require('https')
+
 
 app.use(express.static('dist'))
 
-var ssl = {
-  key: fs.readFileSync("server/assets/server.key", 'utf8'),
-  cert: fs.readFileSync("server/assets/e8f50324691bf90.crt", 'utf8'),
-  ca: fs.readFileSync('server/assets/gd_bundle-g2-g1.crt', 'utf8'),
-};
-server = require('https').createServer(ssl, app);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
